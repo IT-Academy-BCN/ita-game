dcc1ef225c6ab79fc7b6c882c0c81566e668f2c1
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-02-06 17:21:58 +0100

    navbar with navigation link finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import { IconArrow, IconInfo } from "./atoms"
 
 function Navbar({ children }) {
   return (
-    <div className="flex w-full justify-between items-center text-center bg-black text-white p-4  rounded-b-xl">
-      <IconArrow />
+    <div className="flex w-full justify-between items-center text-center bg-black text-white p-4 rounded-b-xl">
+      <Link to={"/"}>
+        <IconArrow />
+      </Link>
       {children}
       <IconInfo />
     </div>
