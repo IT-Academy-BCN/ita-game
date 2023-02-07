079865484e87013c1547c471e0a129e362bf9c16
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-02-07 16:10:53 +0100

    Badges
    Crea e implementa insignias (Badge - atoms) en Profile.

---
+++ src/components/atoms/Badge.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Badge = ({ children, classBody, classFigure, classTitle, img, text }) => {
+   return (
+      <div className="card w-36 bg-base-100">
+         <figure className={classFigure}>
+            <img src={img} alt={children} className="rounded-xl" />
+         </figure>
+         <div className={classBody}>
+            <h6 className={classTitle}>{children}</h6>
+            <p className="text-xs text-gray-400">{text}</p>
+            {/* <p className="text-xs text-gray-400">(+{counter}...)</p> */}
+         </div>
+      </div>
+   );
+};
+
+export default Badge;
