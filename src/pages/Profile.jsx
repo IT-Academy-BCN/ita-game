a2b776d0fe337d5ff2706281088b33b855a76fed
Hiena <aledaldin@gmail.com>
2023-02-08 10:15:22 +0100

    updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,9 +9,9 @@ function Profile() {
       <Navbar>Perfil</Navbar>
       <div className="p-5 gap-4 h-screen">
         <Card>
-          <div className="flex flex-row w-100 justify-center">
+          <div className="flex flex-row w-100 justify-center relative">
             {/* avatar box */}
-            <div className="flex flex-col justify-center items-center w-5/6">
+            <div className="flex flex-col justify-center items-center absolute">
               {/* avatar*/}
               <div className="avatar placeholder">
                 <div className="bg-neutral-focus text-neutral-content rounded-full p-10">
@@ -26,7 +26,7 @@ function Profile() {
               </div>
             </div>
             {/* edit */}
-            <div className="w-1/6">
+            <div className="w-1/6 absolute right-0 ">
               <button className="btn btn-circle bg-primary">
                 <svg
                   width="19"
@@ -43,39 +43,42 @@ function Profile() {
               </button>
             </div>
           </div>
-          {/* progressbar  container*/}
-          <div className="flex flex-col mt-5">
-            {/* bar */}
-            <progress
-              className="progress progress-primary w-100"
-              value="70"
-              max="100"
-            ></progress>
-            {/* data bar */}
-            <div className="flex justify-between">
-              <div className="flex justify-center">
-                <p className="font-bold text-black">250 ITAS </p>
-              </div>
-              <div className="flex justify-center">
-                <p className="font-bold text-black">25 ITAS </p>
-                <p className="text-neutral-focus font-bold ">
-                  para subir de nivel
-                </p>
+          {/* card body */}
+          <div className="flex flex-col  ">
+            {/* progressbar  container*/}
+            <div className="flex flex-col ">
+              {/* bar */}
+              <progress
+                className="progress progress-primary w-100"
+                value="70"
+                max="100"
+              ></progress>
+              {/* data bar */}
+              <div className="flex justify-between">
+                <div className="flex justify-center">
+                  <p className="font-bold text-black text-sm">250 ITAS </p>
+                </div>
+                <div className="flex justify-center">
+                  <p className="font-bold text-black text-sm">25 ITAS </p>
+                  <p className="text-neutral-focus font-bold text-sm">
+                    para subir de nivel
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* divider section */}
-          <div className="flex flex-col">
-            <div className="divider before:bg-secondary after:bg-secondary"></div>
-            <div className="flex justify-between">
-              <p className="text-black text-base">Actividades Realizadas</p>
-              <p className="text-black text-base">48</p>
-            </div>
-            <div className="divider before:bg-secondary after:bg-secondary"></div>
-            <div className="flex justify-between">
-              <p className="text-black text-base">Fecha de alta</p>
-              <p className="text-black text-base">21/12/2022</p>
+            {/* divider section */}
+            <div className="flex flex-col">
+              <div className="divider before:bg-secondary after:bg-secondary"></div>
+              <div className="flex justify-between">
+                <p className="text-black text-base">Actividades Realizadas</p>
+                <p className="text-black text-base">48</p>
+              </div>
+              <div className="divider before:bg-secondary after:bg-secondary"></div>
+              <div className="flex justify-between">
+                <p className="text-black text-base">Fecha de alta</p>
+                <p className="text-black text-base">21/12/2022</p>
+              </div>
             </div>
           </div>
         </Card>
