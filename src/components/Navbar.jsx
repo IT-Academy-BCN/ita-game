95f8f4b0f1a226ceaff52e0429511aa21374a1c1
LUIS VICENTE <bcnluisvi@gmail.com>
2023-02-08 14:54:00 +0100

    fix navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { IconArrow, IconInfo } from './atoms';
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { IconArrow, IconInfo } from "./atoms";
+import { NavLink } from "react-router-dom";
 
 function Navbar({ children }) {
   const [showInfo, setShowInfo] = useState(true);
   const location = useLocation();
 
   useEffect(() => {
-    if (location.pathname === '/information') {
+    if (location.pathname === "/information") {
       setShowInfo(false);
     }
   }, [location]);
 
   return (
     <div className="flex w-full justify-between items-center text-center bg-black text-white p-4 rounded-b-xl">
-      <Link to={'/'}>
+      <Link to={"/"}>
         <IconArrow />
       </Link>
       <div className="flex flex-grow justify-center">{children}</div>
@@ -22,7 +23,7 @@ function Navbar({ children }) {
         <NavLink
           to="/information"
           className={`${
-            location.pathname === '/information' ? 'text-primary' : ''
+            location.pathname === "/information" ? "text-primary" : ""
           }`}
         >
           <IconInfo />
