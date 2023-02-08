b4066bc9e60ae7770d2b14ef67fab48675f883ba
Pabl0Parra <pabsblue16@gmail.com>
2023-02-08 16:38:52 +0100

    add fixed position to Footer

--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { IconsGroup } from './molecules';
+import React from "react";
+import { IconsGroup } from "./molecules";
 
 const FooterMenu = () => {
   return (
-    <div className="absolute bottom-5 left-[4%] bg-black text-secondary h-fit w-[90%] px-5 py-3 rounded-xl">
+    <div className="fixed z-index:1 bottom-5 left-[4%] bg-black text-secondary h-fit w-[90%] px-5 py-3 rounded-xl">
       <IconsGroup />
     </div>
   );
