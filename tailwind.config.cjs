c038ebda496db3198788f533ee8c1e5f6604f1bd
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-09 16:34:10 +0100

    fix-colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,9 +12,9 @@ module.exports = {
           accent: '#B0F41F',
           info: 'black', // background bagdes
           'info-content': 'white', // foreground bagdes
-          'accent-content': '#000000',
-          'base-content': '#1E1E1E', // used for blank backgrounds
-          'base-100': '#969696', // icons
+          // 'accent-content': '#000000',
+          // 'base-content': '#1E1E1E', // used for blank backgrounds
+          // // 'base-100': '#969696', // icons
           warning: '#EFD700',
           'warning-content': '#C6811B'
 
