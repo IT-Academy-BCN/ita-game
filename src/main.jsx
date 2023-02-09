8d4b99e43c16b9c764ee2d4eef60f35590e816e0
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-09 14:01:36 +0100

    console-warm

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import AuthProvider from './store/authentication/AuthContext';
 import App from './App';
 import './index.css';
-
+console.warn('R E C O R D A R 😇  borrar vuestros console.log a la hora de hacer el pull request 🙊🙉🙈')
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
