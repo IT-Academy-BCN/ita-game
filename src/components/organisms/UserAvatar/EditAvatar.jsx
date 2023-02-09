7d32c246b541fe55fb53dc62ee1540caafca4639
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-09 14:31:44 +0100

    edit-open-modal

--- src/components/organisms/UserAvatar/EditAvatar.jsx
+++ src/components/organisms/UserAvatar/EditAvatar.jsx
@@ -1,10 +1,13 @@
+import ButtonEditAvatar from '../../atoms/ButtonEditAvatar'
 import UserAvatar from './UserAvatar'
 
 function EditAvatar() {
-
   return (
     <div className="bg-lime-200">
       PageAvatar
+      <div className="w-1/6 absolute right-0">
+        <ButtonEditAvatar />
+      </div>
       <UserAvatar className="w-32 h-32" userId={2} />
     </div>
   )
