44cdecf3f7577928981dbc9a42f2473452a8c464
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-10 14:53:07 +0100

    page edition demo

---
+++ src/components/avatar/PageEdition.jsx
@@ -0,0 +1,11 @@
+import EditAvatar from "./EditAvatar"
+
+function PageEdition() {
+  return (
+    <div className="flex justify-center p-5 bg-slate-500">
+      <EditAvatar />
+    </div>
+  )
+}
+
+export default PageEdition
