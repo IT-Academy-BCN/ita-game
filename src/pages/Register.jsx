1acedbb5b132904698237fd83b00f80285295ac7
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-10 15:35:35 +0100

    choose avatar

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,27 +1,28 @@
-import { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
-import { inputs } from '../components/atoms/input/Input';
-import Input from '../components/atoms/input/Input';
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import axios from 'axios'
+import { inputs } from '../components/atoms/input/Input'
+import Input from '../components/atoms/input/Input'
 
 // TODO: informar el usuario ha sido o no registrado
 function Register() {
   const [values, setValues] = useState({
     name: '',
     surname: '',
+    sex: '',
     email: '',
     password: '',
-    confirmPassword: '',
-  });
+    confirmPassword: ''
+  })
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const onChange = e => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async e => {
+    e.preventDefault()
     const user = {
       name: values.name,
       surname: values.surname,
@@ -30,18 +31,21 @@ function Register() {
       framework: '',
       ITApoints: 0,
       ITAawards: [],
-      activities: 0
-    };
+      activities: 0,
+      avatar: {
+        sex: values.sex
+      }
+    }
     try {
-      await axios.post('http://localhost:3002/users', user);
-      navigate('/login');
+      await axios.post('http://localhost:3002/users', user)
+      navigate('/login')
     } catch (err) {
-      console.log(err);
+      console.log(err)
       // TODO: implement better way of error handling
     }
-  };
+  }
 
-  const inputArray = inputs(values.password);
+  const inputArray = inputs(values.password)
 
   return (
     <div className="flex min-h-screen justify-center pt-20 px-4">
@@ -60,7 +64,7 @@ function Register() {
           >
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="grid gap-3 rounded-md shadow-sm">
-              {inputArray.map((input) => (
+              {inputArray.map(input => (
                 <Input
                   key={input.id}
                   {...input}
@@ -69,21 +73,30 @@ function Register() {
                 />
               ))}
             </div>
+            <select name="sex" className="input focus:border-primary-500 w-full max-w-xs">
+              <option defaultValue="woman">
+                woman
+              </option>
+              <option value="man">man</option>
+            </select>
             <div className="pt-4">
               <button type="submit" className="btn btn-block btn-primary">
-                  <span className='font-bold'>Registrarme</span> 
+                <span className="font-bold">Registrarme</span>
               </button>
             </div>
           </form>
         </div>
-        <div className='flex justify-center pb-10'>
-          <Link to='/login' className="font-bold text-black hover:text-indigo-500 underline">
+        <div className="flex justify-center pb-10">
+          <Link
+            to="/login"
+            className="font-bold text-black hover:text-indigo-500 underline"
+          >
             ¿Tienes una cuenta?,entrar
           </Link>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Register;
+export default Register
