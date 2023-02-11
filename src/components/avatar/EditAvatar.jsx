ead4e5115134fba73e95e522ed926f3c560e7873
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-11 13:35:17 +0100

    modal-setup

--- src/components/avatar/EditAvatar.jsx
+++ src/components/avatar/EditAvatar.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import ButtonEditAvatar from './ButtonEditAvatar'
+import Modal from './Modal'
 import UserAvatar from './UserAvatar'
 
 function EditAvatar() {
+  const [edition, setEdition] = useState(false)
+  const handleEditAvatar = () => {
+    setEdition(!edition)
+  }
+
   return (
     <div className="relative w-32">
-      <ButtonEditAvatar className="absolute z-10 right-2 -top-3" />
+      <ButtonEditAvatar handleEditAvatar={handleEditAvatar} className="absolute z-10 right-2 -top-3" />
       <UserAvatar />
+      {edition && <Modal />}
     </div>
   )
 }
