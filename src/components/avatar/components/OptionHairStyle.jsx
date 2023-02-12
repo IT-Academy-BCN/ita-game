cb57c9262f8dadb810e6de9ae3717f77007169ea
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-12 20:02:58 +0100

    add hair

--- src/components/avatar/components/OptionHairStyle.jsx
+++ src/components/avatar/components/OptionHairStyle.jsx
@@ -5,10 +5,13 @@ import { actions } from '../store/reducer'
 function OptionHairStyle() {
   const { state, dispatch } = useContext(AvatarContext)
   const { avatar } = state.user
-  
+
   const handleChange = e => {
     const { target } = e
-    dispatch({ type: actions.UPDATE_AVATAR, payload: { ...avatar, hairStyle: target.value } })
+    dispatch({
+      type: actions.UPDATE_AVATAR,
+      payload: { ...avatar, hairStyle: target.value }
+    })
   }
 
   const options = ['normal', 'thick', 'mohawk', 'womanLong', 'womanShort']
@@ -18,7 +21,7 @@ function OptionHairStyle() {
       <label className="label">
         <span className="label-text">Pick the best fantasy HAIR style</span>
       </label>
-      <select onChange={handleChange} className="select select-bordered">
+      <select onChange={handleChange} className="select select-primary select-bordered">
         {options.map(option => (
           <option key={option}>{option}</option>
         ))}
