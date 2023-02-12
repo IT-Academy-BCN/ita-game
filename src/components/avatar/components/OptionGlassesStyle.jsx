5895ac9c7c1cb3508b4585f629fbb1534df136ce
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-12 20:02:49 +0100

    add option glasses

--- src/components/avatar/components/OptionGlassesStyle.jsx
+++ src/components/avatar/components/OptionGlassesStyle.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AvatarContext } from '../store/context'
+import { actions } from '../store/reducer'
 
 function OptionGlassesStyle() {
+  const { state, dispatch } = useContext(AvatarContext)
+  const { avatar } = state.user
+
+  const handleChange = e => {
+    dispatch({
+      type: actions.UPDATE_AVATAR,
+      payload: { ...avatar, glassesStyle: e.target.value }
+    })
+    console.log('from OptionGlasses', e.target.value )
+  }
+  console.log('avatar from option glasses', avatar)
+
   const options = ['none', 'round', 'square']
+
   return (
     <div className="form-control w-full max-w-xs">
       <label className="label">
         <span className="label-text">Pick the best glass style</span>
       </label>
-      <select className="select select-bordered" placeholder="Pick one">
+      <select onChange={handleChange} className="select select-primary select-bordered" placeholder="Pick one">
         {options.map(option => (
           <option key={option}>{option}</option>
         ))}
