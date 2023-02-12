075bc456a474712a16bd4ed8a0a36079bee2446c
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-12 13:44:13 +0100

    add-avatar-edition

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,16 +2,18 @@ import ButtonEditAvatar from './components/ButtonEditAvatar'
 import AvatarEdition from './AvatarEdition'
 import AvatarProvider from './store/context'
 
-function Avatar() {
-
-
+function Avatar({ edit }) {
   return (
     <AvatarProvider>
       <div className="relative w-32">
-        <ButtonEditAvatar
-          className="absolute z-10 right-2 -top-3"
-        />
-        <AvatarEdition />
+        {edit ? (
+          <>
+            <ButtonEditAvatar className="absolute z-10 right-2 -top-3" />
+            <AvatarEdition />
+          </>
+        ) : (
+          <AvatarEdition />
+        )}
       </div>
     </AvatarProvider>
   )
--- src/components/avatar/PageEdition.jsx
+++ src/components/avatar/PageEdition.jsx
@@ -3,6 +3,7 @@ import Avatar from "./Avatar"
 function PageEdition() {
   return (
     <div className="flex justify-center p-5 bg-slate-500">
+      <Avatar edit />
       <Avatar />
       
     </div>
