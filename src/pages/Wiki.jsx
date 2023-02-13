e5ab35376cb4964fda1620deeab470f80341d1bf
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-02-13 18:27:03 +0100

    Wiki NavLink
    Cambio cabecera componente Wiki.
    Implementa iconos .png (borra .svg).
    Añade NavLinks (más hover).

--- src/pages/Wiki.jsx
+++ src/pages/Wiki.jsx
@@ -1,78 +1,66 @@
-import { useState } from 'react';
-import NewResource from '../components/organisms/NewResource';
-import { FooterMenu, Navbar } from '../components';
-import DisplayModeWiki from '../components/DisplayModeWiki';
-import { arrowRight } from '../components/assets';
-import { NavLink } from 'react-router-dom';
 import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 import { WikiContext } from '../store/wikiContext/WikiContext';
+import { FooterMenu, Navbar } from '../components';
+import { Title } from '../components/atoms';
+import DisplayModeWiki from '../components/DisplayModeWiki';
+import folder from '../components/assets/images/new-folder-dynamic-color.png';
+import arrowRight from '../components/assets/arrow_right.svg';
 
 const Wiki = () => {
-  const { categories } = useContext(WikiContext);
-  return (
-    <>
-      <div className="container min-w-full flex flex-col bg-white pb-10">
-        <Navbar>Wiki</Navbar>
-        <div className="card flex flex-row justify-around items-center p-2.5 my-2 mx-6 border border-stone-300">
-          <div className="flex flex-row">
-            <div className="avatar">
-              <div className="w-14 rounded-full px-2 py-2">
-                {/* <img src={image} alt={n} /> */}
-              </div>
+   const { categories } = useContext(WikiContext);
+
+   return (
+      <>
+         <Navbar>Wiki</Navbar>
+         <div className="container min-w-full flex flex-col bg-white pb-10">
+            <div className="bg-slate-200  pt-20">
+               <NavLink to={`/wiki/resource`}>
+                  <div className="card flex  flex-row justify-between h-20 items-center my-2 mx-6 border border-stone-300 bg-white hover:border-stone-400">
+                     <div className="h-full rounded-l-xl rounded-r-3xl  bg-primary w-24 mr-6">
+                        <div className="flex items-center justify-start ml-4 h-full">
+                           <img src={folder} alt="New folder" className="w-2/3" />
+                        </div>
+                     </div>
+                     <div className="w-5/12 flex sm:justify-center">
+                        <h2 className="font-bold text-black text-sm">¿Cómo colaborar en la wiki?</h2>
+                     </div>
+                     <img className="w-6 ml-12 mr-2.5" src={arrowRight} alt="search" />
+                  </div>
+               </NavLink>
+               <div className="form-control mx-6 mt-7 -mb-5 border border-stone-300 rounded-lg">
+                  <div className="input-group flex justify-center">
+                     <input type="text" placeholder="¿Buscas un tema?" className="input input-bordered w-full" />
+                     <button className="btn btn-square">
+                        <svg
+                           xmlns="http://www.w3.org/2000/svg"
+                           className="h-6 w-6"
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           stroke="currentColor"
+                        >
+                           <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                           />
+                        </svg>
+                     </button>
+                  </div>
+               </div>
             </div>
-            <div className="flex flex-col justify-around pl-2 my-1">
-              <h2 className="font-bold text-black">.......</h2>
+
+            <div className="mx-7 mt-10 mb-0">
+               <Title>Categorías</Title>
             </div>
-          </div>
-          <NavLink>
-            <img className="w-6" src={arrowRight} alt="search" />
-          </NavLink>
-        </div>
-        <div className="form-control">
-          <div className="input-group">
-            <input
-              type="text"
-              placeholder="Search…"
-              className="input input-bordered"
-            />
-            <button className="btn btn-square">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-        <div className="flex flex-row justify-between">
-          <p className="flex items-center font-bold m-7 text-black ">
-            Categorías
-          </p>
-          <button className="bg-primary hover:bg-secondary m-7 text-black font-bold  rounded-xl px-2 h-11">
-            + Nuevo recurso
-          </button>
-        </div>
-        {categories.map((c) => (
-          <DisplayModeWiki
-            key={c.id}
-            name={c.stack}
-            resource={c.resource}
-            theme={c.theme}
-          />
-        ))}
-      </div>
-      <FooterMenu />
-    </>
-  );
+            {categories.map((c) => (
+               <DisplayModeWiki key={c.id} name={c.stack} resource={c.resource} theme={c.theme} />
+            ))}
+            <FooterMenu />
+         </div>
+      </>
+   );
 };
 
 export default Wiki;
