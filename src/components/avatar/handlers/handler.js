164fe660c4ce70ace8ebc957c3d49511f5961e3c
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-13 23:20:03 +0100

    remove handlers

--- src/components/avatar/handlers/handler.js
+++
@@ -1,8 +0,0 @@
-const handler = propety => e => {
-  const { target } = e
-  dispatch({
-    type: actions.UPDATE_AVATAR,
-    payload: { ...avatar, propety: target.value }
-  })
-}
-export { handler }
