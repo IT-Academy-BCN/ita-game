ae22e0382164026905c6b8b00095eeb779939f64
Hiena <aledaldin@gmail.com>
2023-02-13 08:50:02 +0100

    cleaned wiki context

--- src/store/wikiContext/WikiContext.jsx
+++ src/store/wikiContext/WikiContext.jsx
@@ -32,7 +32,6 @@ export const WikiContextProvider = ({ children }) => {
       const response = await axios.get(urls.stackData);
       if (response.status === 200) {
         const { data } = response;
-        // aqui guardar toda data para sobreescribir en post?
         const selectedStack = data.filter((d) => d.stack === framework);
         setStackData(selectedStack);
         setStack(framework);
@@ -52,22 +51,6 @@ export const WikiContextProvider = ({ children }) => {
       .then((response) => {
         console.log(response);
       });
-
-    // try {
-    //   const response = await axios.post(urls.stackData);
-    //   if (response.status === 200) {
-    //     const { data } = response;
-    //     const selectedStack = data[framework];
-    //     console.log(selectedStack);
-    //     setStackData(selectedStack);
-    //   }
-    // } catch (error) {
-    //   console.error(error);
-    // }
-
-    // const currentStackData = [stackData];
-    // currentUsers.push(user);
-    // setUsers(currentUsers);
   };
 
   return (
@@ -78,58 +61,3 @@ export const WikiContextProvider = ({ children }) => {
     </WikiContext.Provider>
   );
 };
-
-// import axios from 'axios';
-// import { createContext, useState, useEffect } from 'react';
-// import { urls } from '../../constants/constants';
-
-// export const WikiContext = createContext();
-
-// export const WikiContextProvider = ({ children }) => {
-//   const [categories, setCategories] = useState([]);
-//   const [stackData, setStackData] = useState([]);
-
-//   useEffect(() => {
-//     const getCategories = async () => {
-//       try {
-//         const response = await axios.get(urls.categories);
-//         if (response.status === 200) {
-//           const { data } = response;
-//           setCategories(data);
-//         }
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     getCategories();
-//     getStackData();
-//   }, []);
-
-//   // Funciones de actualización de arr Users en local
-//   // estas funciones no actalizarán con server!!!!!!
-
-//   const getStackData = async (framework) => {
-//     try {
-//       const response = await axios.get(urls.stackData);
-//       if (response.status === 200) {
-//         const { data } = response;
-
-//         setStackData(data);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const addResource = (resource) => {
-//     const currentStackData = [...users];
-//     currentUsers.push(user);
-//     setUsers(currentUsers);
-//   };
-
-//   return (
-//     <WikiContext.Provider value={{ categories, stackData }}>
-//       {children}
-//     </WikiContext.Provider>
-//   );
-// };
