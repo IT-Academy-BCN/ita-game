fd4c65cbd0800e3772436912b956383b321e8a73
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-02-13 13:38:31 +0100

    modal linked to resource

--- src/components/StackReact.jsx
+++ src/components/StackReact.jsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { WikiContext } from '../store/wikiContext/WikiContext';
+import { useContext, useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import { WikiContext } from "../store/wikiContext/WikiContext"
 
 function Stack() {
   // PENDINGS
@@ -10,28 +10,28 @@ function Stack() {
   // check how to get data if NavLink is not pressed
   //on AddResource fetch to post json serve
   //   ckeck: how to access to react and angular objects on JSON serve
-  const [display, setDisplay] = useState([]);
-  const { stackData, getStackData, addResource } = useContext(WikiContext);
-  const location = useLocation();
+  const [display, setDisplay] = useState([])
+  const { stackData, getStackData, addResource } = useContext(WikiContext)
+  const location = useLocation()
 
-  let newdata = location.pathname;
-  let newnewnew = newdata.replace('/', '');
-  console.log(stackData);
+  let newdata = location.pathname
+  let newnewnew = newdata.replace("/", "")
+  console.log(stackData)
 
   useEffect(() => {
     if (stackData.length === 0) {
-      getStackData(newnewnew);
+      getStackData(newnewnew)
     }
-  }, []);
+  }, [])
 
   let fakeData = {
-    id: 'react-200',
-    stack: 'react',
-    title: 'testin',
-    description: 'testing',
-    url: 'https://www.youtube.com/watch?v=gigKP6PPmW0',
+    id: "react-200",
+    stack: "react",
+    title: "testin",
+    description: "testing",
+    url: "https://www.youtube.com/watch?v=gigKP6PPmW0",
     type: { video: true, tutorial: true, blog: false },
-  };
+  }
 
   return (
     <div>
@@ -40,7 +40,7 @@ function Stack() {
       ))}
       <button onClick={() => addResource(fakeData)}> add resource</button>
     </div>
-  );
+  )
 }
 
-export default Stack;
+export default Stack
--- src/components/organisms/NewResource.jsx
+++ src/components/organisms/NewResource.jsx
@@ -1,44 +1,44 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from "react"
 
 const NewResource = ({ setIsModal }) => {
   const [values, setValues] = useState({
-    title: '',
-    description: '',
-    url: '',
+    title: "",
+    description: "",
+    url: "",
     video: false,
     tutorial: false,
     blog: false,
-  });
+  })
 
   useEffect(() => {
-    document.addEventListener('click', handleClickOutside, true);
-  }, []);
-  const clickOutsideRef = useRef(null);
+    document.addEventListener("click", handleClickOutside, true)
+  }, [])
+  const clickOutsideRef = useRef(null)
   const handleClickOutside = (e) => {
     if (!clickOutsideRef.current.contains(e.target)) {
-      setIsModal((prev) => !prev);
+      setIsModal((prev) => !prev)
     }
-  };
+  }
 
   const handleChange = (event) => {
-    const { name, value, type, checked } = event.target;
+    const { name, value, type, checked } = event.target
     setValues((prev) => {
       return {
         ...prev,
-        [name]: type === 'checkbox' ? checked : value,
-      };
-    });
-  };
+        [name]: type === "checkbox" ? checked : value,
+      }
+    })
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     // utilizare addResourse del context para enviar datos de new resourse
   }
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-40 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full bg-black opacity-70 flex justify-center items-center">
       <div ref={clickOutsideRef} className="relative bg-white rounded-2xl z-50">
-        <div className="flex flex-col justify-center p-5 pb-7 rounded-t dark:border-gray-600">
+        <div className="flex flex-col justify-center p-5 pb-7 rounded-t dark:border-secondary">
           <button
             type="button"
             className="btn btn-ghost btn-square absolute top-0 right-0"
@@ -61,7 +61,10 @@ const NewResource = ({ setIsModal }) => {
           </button>
 
           <h2 className="text-center font-black mb-8 mt-6">Nuevo recurso</h2>
-          <form className="flex flex-col items-center gap-3" onSubmit={handleSubmit}>
+          <form
+            className="flex flex-col items-center gap-3"
+            onSubmit={handleSubmit}
+          >
             <label htmlFor="title" className="sr-only">
               Título
             </label>
@@ -102,7 +105,7 @@ const NewResource = ({ setIsModal }) => {
               onChange={handleChange}
             />
             <div className="flex">
-              {' '}
+              {" "}
               {/* checkboxes */}
               <div className="flex items-center">
                 <input
@@ -154,13 +157,13 @@ const NewResource = ({ setIsModal }) => {
               </div>
             </div>
             <button type="submit" className="btn btn-block btn-primary mt-6">
-              <span className="text-gray-900">Crear</span>
+              <span className="text-black">Crear</span>
             </button>
           </form>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NewResource;
+export default NewResource
