0914697fb0ad758b0752689e66a6c348b8947c3b
Nazareno Valentini <nazareno.valentini@gmail.com>
2023-02-13 21:27:40 +0100

    add API call and logic to Leaderboard

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,66 +1,70 @@
 import DisplayModeSmall from "../components/DisplayModeSmall";
 import DisplayMode from "../components/DisplayMode";
 import { FooterMenu, Navbar } from "../components";
-import db from "../../dbLeaderBoard.json";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { DateTime } from "luxon";
 
 const currentUser = {
-  name: "Homer Simpson",
-  points: 100,
+  id: "63e9d29bb04cb600417abcb6",
+  name: "Ona Costa",
+  points: 80,
+};
+const url = "https://itacademy.onrender.com/api/activity/";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2U5ZDA1OGIwNGNiNjAwNDE3YWJjYWUiLCJpYXQiOjE2NzYyNjc3MjZ9.4NFtPYgOQnQbWeAQ3Ow0qhyeMszw8cqC5TlOBRlaynM";
+const options = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 };
 
-const users = [
-  { name: "Homer Simpson", points: 100 },
-  { name: "Marge Simpson", points: 130 },
-  { name: "Lisa Simpson", points: 190 },
-  { name: "Bart Simpson", points: 115 },
-  { name: "Barney Gumble", points: 78 },
-];
-
 function LeaderBoard() {
-  // 1. filtrar las actividades de la ultima semana
-  // 2. crear un array de usuarios con:
-  //  nombre
-  //  puntos
-  //  nivel???
-  // 3. ordenarlo
+  const [users, SetUsers] = useState();
 
-  // let arrayC = [{ name: "Albert García" }];
-  // db.forEach((user) => {
-  //   const found = arrayC.find((e) => e.name === user.doneBy.name);
-  //   console.log("found::", found);
-  // });
+  useEffect(() => {
+    const weekTimestamp = Math.floor(Date.now() / 1000);
+    const weekDate = DateTime.fromMillis(weekTimestamp * 1000);
+    const startOfWeek = weekDate.startOf("week");
+    let arrayCategories = [];
+    // llamamos la API que nos devuelve un array con todas las actividades
+    const getCategoriesArray = async () => {
+      try {
+        const response = await axios.get(url, options);
+        if (response.status === 200) {
+          const { data } = response;
+          arrayCategories = data;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      let users = [];
+      arrayCategories
+        // filtramos per la ultima semana
+        .filter((item) => {
+          const itemDate = DateTime.fromMillis(Date.parse(item.doneDate));
+          return (
+            itemDate >= startOfWeek && itemDate < startOfWeek.plus({ weeks: 1 })
+          );
+        })
+        // creamos el array users que vamos a usar para renderizar
+        .forEach((user) => {
+          const found = users.findIndex((e) => e.id === user.doneBy._id);
+          if (found === -1) {
+            users.push({
+              id: user.doneBy._id,
+              name: user.doneBy.name,
+              points: user.typeId.points,
+            });
+          } else {
+            users[found].points += user.typeId.points;
+          }
+          SetUsers(users);
+        });
+    };
+    getCategoriesArray();
+  }, []);
 
-  // let arrayC = [];
-  // db.forEach((user) => {
-  //   const found = arrayC.find((e) => e.id === user.doneBy._id);
-  //   if (!found) {
-  //     arrayC.push({
-  //       id: user.doneBy._id,
-  //       name: user.doneBy.name,
-  //       score: user.typeId.points,
-  //     });
-  //   } else {
-
-  //   }
-  // });
-
-  let arrayC = [];
-  db.forEach((user) => {
-    const found = arrayC.findIndex((e) => e.id === user.doneBy._id);
-    if (found === -1) {
-      arrayC.push({
-        id: user.doneBy._id,
-        name: user.doneBy.name,
-        points: user.typeId.points,
-      });
-    } else {
-      arrayC[found].points += user.typeId.points;
-    }
-  });
-
-  //console.log("db[0].doneBy.name: ", db[0].doneBy.name);
-  // console.log("db: ", db);
-  console.log("arrayC: ", arrayC);
   return (
     <>
       <Navbar>Competición</Navbar>
@@ -69,18 +73,19 @@ function LeaderBoard() {
         <p className="text-lg font-semibold m-6 text-black">
           Clasificación semanal
         </p>
-        {users
-          // aqui filtrar solo los de la ultima semana
-          .sort((a, b) => (a.points > b.points ? -1 : 0)) // ordenamos por puntuacion
-          .map((user, index) => (
-            <DisplayModeSmall
-              key={user.name}
-              user={user}
-              position={index + 1}
-              currentUser={currentUser}
-              level={5}
-            />
-          ))}
+        {users &&
+          users
+            // ordenamos por puntuacion
+            .sort((a, b) => (a.points > b.points ? -1 : 0))
+            .map((user, index) => (
+              <DisplayModeSmall
+                key={user.name}
+                user={user}
+                position={index + 1}
+                currentUser={currentUser}
+                level={5}
+              />
+            ))}
       </div>
       <FooterMenu />
     </>
