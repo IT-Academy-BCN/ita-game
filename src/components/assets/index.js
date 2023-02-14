1a9373fd32bda57d4d77a6d3f2528efe3336ff5a
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-02-14 08:53:16 +0100

    Profile & Badge
    Implementa iconos .png (borra .svg).

--- src/components/assets/index.js
+++
@@ -1,4 +0,0 @@
-export { default as folder } from './folder-dynamic.svg';
-export { default as gym } from './gym-dynamic-clay.svg';
-export { default as rocket } from './rocket-dynamic.svg';
-export { default as sun } from './sun-dynamic.svg';
