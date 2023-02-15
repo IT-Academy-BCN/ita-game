e92af2bb85d1bdc4713aa039967a94fcfb982a90
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-02-15 09:41:19 +0100

    new context

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AuthProvider from './store/authentication/AuthContext';
-import App from './App';
-import './index.css';
-import { WikiContextProvider } from './store/wikiContext/WikiContext';
-import { ActivitiesContextProvider } from './store/activitiesContext/ActivitiesContext';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import AuthProvider from "./store/authentication/AuthContext"
+import App from "./App"
+import "./index.css"
+import { WikiContextProvider } from "./store/wikiContext/WikiContext"
+import { ActivitiesContextProvider } from "./store/activitiesContext/ActivitiesContext"
+import {
+  UsersContext,
+  UsersContextProvider,
+} from "./store/usersApi/UsersContext"
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <WikiContextProvider>
-        <ActivitiesContextProvider>
-        <App />
-        </ActivitiesContextProvider>
-      </WikiContextProvider>
+      <UsersContextProvider>
+        <WikiContextProvider>
+          <ActivitiesContextProvider>
+            <App />
+          </ActivitiesContextProvider>
+        </WikiContextProvider>
+      </UsersContextProvider>
     </AuthProvider>
   </React.StrictMode>
-);
+)
--- src/store/usersApi/UsersContext.jsx
+++ src/store/usersApi/UsersContext.jsx
@@ -4,26 +4,41 @@ import { apiUrl } from "../../constants/constants"
 
 export const UsersContext = createContext()
 
+const currentUser = {
+  id: "63e9d29bb04cb600417abcb6",
+  name: "Ona Costa",
+  points: 80,
+}
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2U5ZDA1OGIwNGNiNjAwNDE3YWJjYWUiLCJpYXQiOjE2NzYyNjc3MjZ9.4NFtPYgOQnQbWeAQ3Ow0qhyeMszw8cqC5TlOBRlaynM"
+const options = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+}
+
 export const UsersContextProvider = ({ children }) => {
   const [user, setUser] = useState([])
 
-  const getUser = async () => {
-    try {
-      const resp = await axios.get(
-        `https://itacademy.onrender.com/api/users/${id}`
-      )
-      if (resp.status === 200) {
-        const { data } = resp
-        setUser(data)
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const resp = await axios.get(
+          `${apiUrl.users}/${currentUser.id}`,
+          options
+        )
+        if (resp.status === 200) {
+          const { data } = resp
+          setUser(data)
+        }
+      } catch (error) {
+        console.log(error)
       }
-    } catch (error) {
-      console.log(error)
     }
-  }
+    getUser()
+  }, [])
 
   return (
-    <UsersContext.Provider value={{ getUser }}>
-      {children}
-    </UsersContext.Provider>
+    <UsersContext.Provider value={{ user }}>{children}</UsersContext.Provider>
   )
 }
