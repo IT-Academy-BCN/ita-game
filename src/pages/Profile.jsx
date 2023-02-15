4eaace5be89955986446c7e7f393bd5effb3ab87
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-15 10:59:35 +0100

    calcultate totalpoints

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import user from '../components/profile/mocks/user.json'
+import activities from '../components/profile/mocks/activities.json'
 import { FooterMenu, Navbar } from '../components'
 import folder from '../assets/images/rocket-dynamic.png'
 import rocket from '../assets/images/rocket-dynamic.png'
@@ -11,16 +12,23 @@ import CardProfile from '../components/profile/CardProfile'
 // LOGIC: refactor into smaller components/ endpoint Itaawards with images/Protecte Route
 // UI:Grid on 'Insignias Ganadas' Cards
 
+
 function Profile() {
+  const totalPoints = activities
+    .filter(activity => activity.doneBy._id === user._id)
+    .map(doneActivities => doneActivities.typeId.points)
+    .reduce((acc, current) => acc + current, 0)
+
   // data from the user.json
   const data = {
     name: user.name,
     surname: user.surname,
     framework: user.framework,
-    points: user.ITApoints,
+    points: totalPoints,
     activities: user.activities,
     multiple: Math.ceil(user.ITApoints / 50) * 50
   }
+
   // Fake user to obtain SVG and Data for looping
   const fakeUsers = {
     ITAawards: [
