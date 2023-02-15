8fcbbe3f360a74731f7383241eaf0819f0f2f3f4
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-15 11:32:42 +0100

    add status progress bar

--- src/components/profile/ProgressBar.jsx
+++ src/components/profile/ProgressBar.jsx
@@ -1,10 +1,12 @@
-function ProgressBar({ data:{points} }) {
-  const nextMultiple = Math.ceil(points / 50) * 50
+function ProgressBar({ data: { points } }) {
+  const level = Math.ceil(points / 50);
   return (
     <div>
+      <p className="uppercase font-bold">Nivel {level}</p>
+
       <progress
-        className="progress progress-primary bg-slate-200" 
-        value={(points * 100) / nextMultiple}
+        className="progress progress-primary bg-slate-200"
+        value={(points % 50) * 2}
         max={100}
       >
         {points}%
