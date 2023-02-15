dd0bcbde7aea930aad447a343b591616a5fc03d7
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-02-15 10:40:17 +0100

    Calender
    Refactoriza Admin con componente Calender.

---
+++ src/components/Calender.jsx
@@ -0,0 +1,43 @@
+import { forwardRef, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import calender from '../assets/images/calender-dynamic-color.png';
+import arrowDown from '../assets/arrow_down.svg';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+const Calender = ({ startDate, setStartDate }) => {
+   const CalenderInput = forwardRef(({ onClick }, ref) => (
+      <div
+         className="card flex flex-row justify-between items-center p-2.5 my-2 mx-6 border border-stone-300 hover:border-stone-400 bg-white"
+         onClick={onClick}
+         ref={ref}
+      >
+         <div className="flex flex-row">
+            <div className="avatar">
+               <div className="w-14 rounded-full bg-slate-100 px-2 py-2">
+                  <img src={calender} alt="calender" />
+               </div>
+            </div>
+            <div className="flex flex-col justify-center pl-2 my-1">
+               <h2 className="font-bold text-black">Hoy</h2>
+            </div>
+         </div>
+         <div>
+            <img className="w-6" src={arrowDown} alt="Arrow down" />
+         </div>
+      </div>
+   ));
+
+   return (
+      <DatePicker
+         // portalId="root-portal"
+         selected={startDate}
+         onChange={(date) => setStartDate(date)}
+         customInput={<CalenderInput />}
+         // dateFormat="yyyy-MM-dd"
+         dateFormat="MMMM d, yyyy h:mm aa"
+      />
+   );
+};
+
+export default Calender;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,48 +1,24 @@
-import { forwardRef, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { FooterMenu, Navbar } from '../components';
 import arrowDown from '../assets/arrow_down.svg';
 import { Title } from '../components/atoms';
 import avatar from '../assets/images/avatar.png';
 import thumbUp from '../assets/images/thumb-up-dynamic-color.png';
-import calender from '../assets/images/calender-dynamic-color.png';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import Calender from '../components/Calender';
 
 const fakeAdmin = [
    { id: 'Ona Costa', img: avatar },
    { id: 'Duda Resuelta', img: thumbUp }
-   // { id: 'Hoy', img: calender }
 ];
 
 const Admin = () => {
    const [startDate, setStartDate] = useState(new Date());
+   // console.log('startDate', typeof startDate.toISOString());
    const hiddenBrowseButton = useRef(null);
-
-   const CalenderInput = forwardRef(({ onClick }, ref) => (
-      <div
-         className="card flex flex-row justify-between items-center p-2.5 my-2 mx-6 border border-stone-300 hover:border-stone-400 bg-white"
-         onClick={onClick}
-         ref={ref}
-      >
-         <div className="flex flex-row">
-            <div className="avatar">
-               <div className="w-14 rounded-full bg-slate-100 px-2 py-2">
-                  <img src={calender} alt="calender" />
-               </div>
-            </div>
-            <div className="flex flex-col justify-center pl-2 my-1">
-               <h2 className="font-bold text-black">Hoy</h2>
-            </div>
-         </div>
-         <div>
-            <img className="w-6" src={arrowDown} alt="Arrow down" />
-         </div>
-      </div>
-   ));
 
    return (
       <>
-         <Navbar>Competición</Navbar>
+         <Navbar>Home (Admin)</Navbar>
          <div className="container min-w-full flex flex-col bg-slate-100 pb-10">
             <div className="mx-7 mt-20 mb-0">
                <h1 className="text-3xl font-bold text-black">¡Hola Manoli!</h1>
@@ -50,7 +26,6 @@ const Admin = () => {
             <div className="mx-7 mt-10 mb-0">
                <Title>Añadir nueva actividad</Title>
             </div>
-            {/*  */}
             {fakeAdmin.map((f) => {
                return (
                   <div
@@ -73,13 +48,7 @@ const Admin = () => {
                   </div>
                );
             })}
-            <DatePicker
-               // portalId="root-portal"
-               selected={startDate}
-               onChange={(date) => setStartDate(date)}
-               customInput={<CalenderInput />}
-               dateFormat="yyyy-MM-dd"
-            />
+            <Calender startDate={startDate} setStartDate={setStartDate} />
             <div className="flex justify-center mb-3">
                <button type="submit" className="btn  btn-primary mt-6 w-80 px-16">
                   <span className="text-gray-900">Enviar</span>
