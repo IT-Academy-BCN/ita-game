7af78895eaf0860ff2aebe1e009c8a47142fa990
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-16 22:31:12 +0100

    fix negative points

--- src/components/profile/SubLabelProgressBar.jsx
+++ src/components/profile/SubLabelProgressBar.jsx
@@ -12,7 +12,7 @@ function SubLabelProgressBar({ subLabeData: { points } }) {
       </div>
       <div className="flex justify-center">
         <p className="font-bold text-black text-sm">
-          {points - level * 50}
+          {level * 50 - points}
           <span className="ml-2 text-neutral-focus font-bold text-sm">
             para subir de nivel
           </span>
