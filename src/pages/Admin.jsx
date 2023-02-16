4afdd0079ad9dc1050342ed9113caf2fc02622ba
Laura-Ruiz <laaura.26@gmail.com>
2023-02-16 11:29:41 +0100

    update

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -22,39 +22,45 @@ const options = {
   },
 };
 
+const categories = [
+  { id: 1, stack: "React" },
+  { id: 2, stack: "Vue" },
+  { id: 3, stack: "Angular" },
+];
+
+const activities = [
+  { id: 1, type: "Duda Resuelta", img: thumbUp },
+  { id: 2, type: "wiki", img: thumbUp },
+  { id: 3, type: "explicación en clase", img: thumbUp },
+];
+
 const Admin = () => {
   // const { categories } = useContext(WikiContext);
 
-  const users = useUsers(URL_USERS)
-  const categories = [
-    { id: 1, stack: "React" },
-    { id: 2, stack: "Vue" },
-    { id: 3, stack: "Angular" },
-  ];
+  const users = useUsers(URL_USERS);
 
-  const activities = [
-   { id:1, type: "Duda Resuelta", img: thumbUp },
-   { id: 2, type:"wiki", img: thumbUp },
-   { id:3, type: "explicación en clase", img: thumbUp }
- ];
+  // Hacer filtro con el selectedStack.stack de users 
 
-
-  const [selectedStack, setSelectedStack] = useState(null)
-  const [selectedUser, setSelectedUser] = useState(null)
-  const [selectedActivity, setSelectedActivity] = useState(null)
+  const [selectedStack, setSelectedStack] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [selectedActivity, setSelectedActivity] = useState(null);
 
   const [startDate, setStartDate] = useState(null);
 
-   const [data, setData] = useState()
+  const [data, setData] = useState();
 
-   useEffect(() => {
-   const newData = { selectedStack, selectedUser, selectedActivity, startDate };
-   setData(newData);
- }, [selectedStack, selectedUser, selectedActivity, startDate]);
+  useEffect(() => {
+    if(selectedActivity){
+      const newData = {
+        type: selectedActivity.type,
+        date: startDate,
+      };
+      setData(newData);
+    }
+   
+  }, [selectedStack, selectedUser, selectedActivity, startDate]);
 
-  // console.log('startDate', typeof startDate.toISOString());
   const hiddenBrowseButton = useRef(null);
-
 
   const handleClick = () => {
     const elem = document.activeElement;
@@ -63,16 +69,18 @@ const Admin = () => {
     }
   };
 
-const handleSubmit = async(e) => {
-   console.log("HOLA")
-   e.preventDefault();
-try {
-   await axios.post("", data)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        `https://itacademy.onrender.com/api/activity/new/${selectedUser._id}`,
+        data
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-} catch (error) {
-   console.log(error)
-}
-}
   return (
     <>
       <Navbar>Home (Admin)</Navbar>
@@ -93,13 +101,19 @@ try {
             <div className="avatar">
               <div className="w-14 rounded-full px-2 py-2">
                 <img
-                  src={import.meta.resolve(`../assets/images/${selectedStack ? selectedStack.stack : "angular"}.png`)}
+                  src={import.meta.resolve(
+                    `../assets/images/${
+                      selectedStack ? selectedStack.stack : "angular"
+                    }.png`
+                  )}
                   alt="Angular logo"
                 />
               </div>
             </div>
             <div className="flex flex-col justify-center pl-2 my-1">
-              <h2 className="font-bold text-black">{selectedStack ? selectedStack.stack : "Categoria"}</h2>
+              <h2 className="font-bold text-black">
+                {selectedStack ? selectedStack.stack : "Categoria"}
+              </h2>
             </div>
           </div>
           <div>
@@ -114,9 +128,10 @@ try {
                 className="card flex flex-row justify-between items-center p-2 my-1 mx-6 border border-stone-300 hover:border-stone-400 bg-white"
                 key={c.id}
                 value={c.id}
-                onClick={() => { 
-                  handleClick()
-                  setSelectedStack(c)}}
+                onClick={() => {
+                  handleClick();
+                  setSelectedStack(c);
+                }}
               >
                 <div className="flex flex-row">
                   <div className="avatar">
@@ -154,7 +169,9 @@ try {
               </div>
             </div>
             <div className="flex flex-col justify-center pl-2 my-1">
-              <h2 className="font-bold text-black">{selectedUser ? selectedUser.name : "Alumno"}</h2>
+              <h2 className="font-bold text-black">
+                {selectedUser ? selectedUser.name : "Alumno"}
+              </h2>
             </div>
           </div>
           <div>
@@ -170,9 +187,9 @@ try {
                 key={u._id}
                 value={u._id}
                 onClick={() => {
-                  handleClick()
-                  setSelectedUser(u)
-               }}
+                  handleClick();
+                  setSelectedUser(u);
+                }}
               >
                 <div className="flex flex-row">
                   <div className="avatar">
@@ -202,11 +219,16 @@ try {
           <div className="flex flex-row">
             <div className="avatar">
               <div className="w-14 rounded-full px-2 py-2">
-                 <img src={selectedActivity ? selectedActivity.img : thumbUp} alt={`Logo actividad`} />
+                <img
+                  src={selectedActivity ? selectedActivity.img : thumbUp}
+                  alt={`Logo actividad`}
+                />
               </div>
             </div>
             <div className="flex flex-col justify-center pl-2 my-1">
-              <h2 className="font-bold text-black">{selectedActivity ? selectedActivity.type : "Actividad"}</h2>
+              <h2 className="font-bold text-black">
+                {selectedActivity ? selectedActivity.type : "Actividad"}
+              </h2>
             </div>
           </div>
           <div>
@@ -221,17 +243,15 @@ try {
                 className="card flex flex-row justify-between items-center p-2 my-1 mx-6 border border-stone-300 hover:border-stone-400 bg-white"
                 key={a.id}
                 value={a.id}
-                onClick={() => { 
-                  handleClick()
-                  setSelectedActivity(a)}}
+                onClick={() => {
+                  handleClick();
+                  setSelectedActivity(a);
+                }}
               >
                 <div className="flex flex-row">
                   <div className="avatar">
                     <div className="w-14 rounded-full bg-slate-100 px-2 py-2">
-                      <img
-                        src={a.img}
-                        alt={`${a.type}-logo`}
-                      />
+                      <img src={a.img} alt={`${a.type}-logo`} />
                     </div>
                   </div>
                   <div className="flex flex-col justify-center pl-2 my-1">
@@ -244,8 +264,6 @@ try {
             ))}
           </ul>
         </div>
-
-
 
         {/* {fakeAdmin.map((f) => {
           return (
@@ -270,13 +288,14 @@ try {
           );
         })} */}
 
-
         {/* CALENDER */}
         <Calender startDate={startDate} setStartDate={setStartDate} />
 
         <div className="flex justify-center mb-3">
           <button type="submit" className="btn  btn-primary mt-6 w-80 px-16">
-            <span onClick={handleSubmit} className="text-gray-900">Enviar</span>
+            <span onClick={handleSubmit} className="text-gray-900">
+              Enviar
+            </span>
           </button>
         </div>
         <div className="flex justify-center mb-10">
